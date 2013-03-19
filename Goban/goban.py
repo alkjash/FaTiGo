a93bd3a37b43f7ae523ca0353deb420633d72b6c
Xiaoyu He <alkjash876@gmail.com>
2013-03-18 22:09:36 -0400

    Added dummy code for goban

---
+++ Goban/goban.py
@@ -0,0 +1,28 @@
+class Board:
+	"""Board class that keeps track of moves, captures, and score"""
+
+	# Initialize empty board with initial state
+	# Should be augmented to keep track of captures,
+	# Raw scores, etc.
+	def __init__(self): 
+		pass
+
+	# Play a move as black
+	def play_black(self, x, y):
+
+		pass
+
+	# Play a move as white
+	def play_white(self, x, y):
+
+		pass
+
+	# Return score of white, assuming game finished
+	def white_score(self):
+		
+		pass
+
+	# Return score of black, assuming game finished
+	def black_score(self):
+	
+		pass
